19437e6c68469debe96aaa06a7216c0309c431a6
Matt Moore <mattmoore@carbonhelix.com>
2017-10-23 02:57:43 -0500

    Release photon 0.1.7

--- photon.rb
+++ photon.rb
@@ -1,11 +1,11 @@
 require "formula"
 
 class Photon < Formula
-  version '0.1.6'
+  version '0.1.7'
   desc "Network helper utility, similar to curl but with built-in API authentication."
   homepage "https://github.com/mattmoore/photon"
   url "https://github.com/mattmoore/photon/releases/download/#{version}/photon-#{version}.macos.sierra.tar.xz"
-  sha256 "29d0feb525745865b91bfcb759c8feb0371ac7cebacd0593685ad2f4c74e00ba"
+  sha256 "385a5272fdc0bfccd12d84f0610c618953a3a4a8faa4cdc85e5998a1bb07fcdf"
 
   def install
     bin.install "photon"
